4522fd8dbd611996d02cc2591917e6379d3558d0
huynhdev24 <huynh.dev.24@gmail.com>
2023-03-28 12:35:01 +0700

    refactor routers

--- routes/publisher.js
+++ routes/publisher.js
@@ -1,14 +1,11 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const publisherController = require('../controllers/publishers.controller');
 
-const publisherController = require('../controllers/publishers.controller')
-
-
-router.get('/', publisherController.getAll)
-router.get('/:id', publisherController.getById)
-router.post('/', publisherController.create)
-router.put('/:id', publisherController.updateById)
-router.delete('/:id', publisherController.deleteById)
-
+router.get('/', publisherController.getAll);
+router.get('/:id', publisherController.getById);
+router.post('/', publisherController.create);
+router.put('/:id', publisherController.updateById);
+router.delete('/:id', publisherController.deleteById);
 
 module.exports = router;
