59285797e5ac1976215852bd3343d2b43fff7b7d
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-06 18:43:49 +0700

    remove comment history controller

--- controllers/history.controller.js
+++ controllers/history.controller.js
@@ -3,34 +3,6 @@ const historyService = require('../services/history.service');
 const historyController = {
     // lấy danh sách lịch sử có phân trang
     getAll: async(req, res) => {
-        // try {
-        //     const page = req.query.page ? parseInt(req.query.page) : 1
-        //     const limit = req.query.limit ? parseInt(req.query.limit) : 0
-        //     const sort = req.query.sort ? req.query.sort : { createdAt: -1 }
-        //     const { query } = req.query
-
-        //     const queryObj = !!query ? query : {}
-            
-        //     const [count, data] = await historyService.getAll({query: queryObj, page, limit, sort})
-        //     const totalPage = Math.ceil(count / limit)
-            
-        //     res.status(200).json({
-        //         message: 'success',
-        //         error: 0,
-        //         data,
-        //         count,
-        //         pagination: {
-        //             page,
-        //             limit,
-        //             totalPage,
-        //         }
-        //     })
-        // } catch (error) {
-        //     res.status(500).json({
-        //         message: `Có lỗi xảy ra! ${error.message}`,
-        //         error: 1,
-        //     })
-        // }
         try {
             const page = req.query.page ? parseInt(req.query.page) : 1
             const limit = req.query.limit ? parseInt(req.query.limit) : 2
